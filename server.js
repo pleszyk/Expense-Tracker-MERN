9a26df483d1b9aff7abf13942c5329ae8e831002
Paul Leszyk <pleszyk@gmail.com>
2022-11-18 16:07:21 -0500

    deploy

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
-const connectDB = require('./config/db');
+const connectDB = require('./db');
 
-dotenv.config({ path: './config/config.env' });
+dotenv.config();
 
 connectDB();
 
